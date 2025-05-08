530eccaee565d8751d3ad4baa2e77ff8a01be6a7
aacedeno <aacedeno1@gmail.com>
2025-05-07 21:54:25 -0500

    Initial commit for todo app and folder management

---
+++ todo_app/main.py
@@ -0,0 +1,15 @@
+#Prompting user to enter input
+user_prompt = "Enter todo: "
+
+#Storing user prompts
+todos = []
+
+while True:
+    todo = input(user_prompt)
+    print(todo.capitalize())
+    todos.append(todos)
+
+
+
+
+
